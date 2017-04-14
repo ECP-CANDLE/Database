de697bc13b524382edfe02a95ccc80e03a7571b6
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-14 11:42:04 -0500

    Be verbose

--- examples/insert-run.sh
+++ examples/insert-run.sh
@@ -4,6 +4,6 @@ set -eu
 # INSERT RUN
 # Trying to imitate https://cwiki.apache.org/confluence/display/solr/Uploading+Data+with+Index+Handlers
 
-curl -X POST -H 'Content-Type: application/json' \
-     'http://localhost:8983/solr/run/update/json/docs' --data-binary \
+curl -v -X POST -H 'Content-Type: application/json' \
+     'http://localhost:8983/solr/run/update/json/docs' --data-ascii \
      $*
