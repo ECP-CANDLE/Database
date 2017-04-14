ddc7c85f7fb045ffe28599f13479f67518040cd9
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-14 12:37:51 -0500

    Retrieve all runs

--- examples/query-run.sh
+++ examples/query-run.sh
@@ -4,4 +4,4 @@ set -eu
 # QUERY RUN
 # Attempt to find run_id:1 .  Produces numFound="0"
 
-wget -q -O - "http://localhost:8983/solr/run/select?q=run_id:1"
+wget -q -O - "http://localhost:8983/solr/run/select?q=run_id:*"
