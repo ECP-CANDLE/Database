a549b8f8cf47c86b3147fd75607ba4aafaac365b
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-17 11:44:37 -0500

    Works- switch to JSON output

--- examples/run-query.sh
+++ examples/run-query.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eu
 
-# QUERY RUN
-# Attempt to find run_id:1 .  Produces numFound="0"
+# RUN QUERY
+# For run_id
 
-wget -q -O - "http://localhost:8983/solr/run/select?q=run_id:*"
+# wget -q -O - "http://localhost:8983/solr/run/select?q=run_id:*"
+
+# Request JSON output:
+wget -q -O - "http://localhost:8983/solr/run/select?q=run_id:*&wt=json"
