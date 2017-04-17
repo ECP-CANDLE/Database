10143bddda13ea076d14e0945906115e7baee6d1
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-17 11:41:44 -0500

    More arguments

--- py/run_insert.py
+++ py/run_insert.py
@@ -8,11 +8,29 @@ import pysolr
 # Setup a Solr instance. The timeout is optional.
 solr = pysolr.Solr('http://localhost:8983/solr/run', timeout=10)
 
-def run_insert(run_id, parameters):
+def run_insert(run_id, parameters,
+               benchmark_id  = "unknown",
+               dataset_id    = "unknown",
+               experiment_id = "unknown",
+               start_time    = None,
+               end_time      = None,
+               runtime_hours = None,
+               status        = "SUCCESS",
+               run_progress  = None,
+               training_accuracy = None,
+               training_loss = None,
+               validation_accuracy = None,
+               validation_loss = None,
+               model_checkpoint_file = None,
+               model_description_file = None,
+               model_weight_file = None,
+               model_result_files = None):
     solr.add([
         {
             "run_id": run_id,
             "parameters": parameters,
+            "benchmark_id": benchmark_id
+
         }])
 
 if __name__ == "__main__":
