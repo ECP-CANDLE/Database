daf792d07f5241adbf62ff5a83db902628345639
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-17 11:41:55 -0500

    New JSON pretty-printer

---
+++ examples/json_print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# JSON PRINT
+# Simply pipe JSON in here for pretty-printing
+
+import json
+import sys
+
+json_data = json.load(sys.stdin)
+print json.dumps(json_data, indent=2, sort_keys=True)
