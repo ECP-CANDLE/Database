ed64e499ae2967ae2b066ed549afbe22fb5c4f2d
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-19 12:48:28 -0500

    Validate input

--- examples/run-insert.sh
+++ examples/run-insert.sh
@@ -4,6 +4,12 @@ set -eu
 # INSERT RUN
 # Trying to imitate https://cwiki.apache.org/confluence/display/solr/Uploading+Data+with+Index+Handlers
 
+if [[ ${#} == 0 ]]
+then
+  echo "Requires JSON input!"
+  exit 1
+fi
+
 curl -H 'Content-Type: application/json' \
      'http://localhost:8983/solr/run/update/json/docs?commit=true' --data-ascii \
      $*
