aff09489f4f93469f5131017a5101351a4e1c5c2
Justin Wozniak <wozniak@mcs.anl.gov>
2017-07-04 10:00:53 -0500

    Add header; restore PDF creation

--- plots/plot.sh
+++ plots/plot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 
+# PLOT SH
+
+# Wraps JWPLOT, EPS->PDF conversion
+# Provide a MODE: load or rate
+
 if [[ ${#*} != 1 ]]
 then
   echo "Provide MODE!"
@@ -10,4 +15,4 @@ MODE=$1
 
 # JWPLOT: https://github.com/jmjwozniak/jwplot
 jwplot $MODE.{cfg,eps,data}
-# convert $MODE.{eps,pdf}
+convert $MODE.{eps,pdf}
