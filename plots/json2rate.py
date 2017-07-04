5971a23387eee40e1cec62852b399f3bdb0b2396
Justin Wozniak <wozniak@mcs.anl.gov>
2017-07-04 09:33:32 -0500

    New json2rate

---
+++ plots/json2rate.py
@@ -0,0 +1,81 @@
+
+# JSON TO RATE
+
+# NOTE: Run this with ./json2rate (shell script wrapper)
+
+# Converts a directory of runs into a learning rate over time table
+# The output file format for each line is
+# timestamp rate
+# where the timestamp is floating point Unix hours
+#   relative to first event
+# and the rate is:
+#   trainable_params * ceiling(1120/batch_size) * epochs run / total time
+
+# Tested with dataset on Theta:
+# root_dir=/projects/Candle_ECP/pbalapra/Experiments/nt3_mlrMBO-360-10
+
+import math
+
+# Set PYTHONPATH=$PWD
+from plottools import *
+
+(root_dir, output_file) = get_args()
+
+# List of all run directories to be processed
+rundirs = get_rundirs(root_dir)
+
+# List of events, each a tuple: (timestamp, START|STOP)
+events = []
+
+def get_param(parameters, param):
+    token = param+": "
+    chars = len(token)
+    for parameter in parameters:
+        if parameter.startswith(token):
+            s = parameter[chars:]
+            return int(s)
+
+for rundir in rundirs:
+    J = get_json(rundir)
+    record_count = len(J)
+    record_start = J[0]
+    record_stop  = J[record_count-1]
+    epochs = record_count-2
+    parameters = J[0]["parameters"]
+    batch_size       = get_param(parameters, "batch_size")
+    trainable_params = get_param(parameters, "trainable_params")
+    time_str_start = record_start["start_time"]
+    time_str_stop  = record_stop ["end_time"]["set"]
+    secs_start = date2secs(time_str_start)
+    secs_stop  = date2secs(time_str_stop)
+    duration = secs_stop - secs_start
+    rate = trainable_params * math.ceil(1120.0/batch_size) * epochs / duration
+    # Store the event tuples
+    events.append((secs_start,START,rate))
+    events.append((secs_stop,STOP,rate))
+
+print("Found %i events." % len(events))
+
+# Sort by timestamp
+events.sort()
+
+# Get first event time
+first = events[0][0]
+
+fp_out = open(output_file, "w")
+fp_out.write("0 0\n")
+
+# Run through the events in timestamp order,
+# record rate at each transition point
+rate = 0
+for event in events:
+    t = (event[0] - first) / 3600
+    if event[1] == START:
+        rate = rate+event[2]
+    elif event[1] == STOP:
+        rate = rate-event[2]
+    else:
+        abort("Unknown event!")
+    fp_out.write("%0.6f %i\n" % (t, rate))
+
+fp_out.close()
--- plots/plottools.py
+++ plots/plottools.py
@@ -3,7 +3,60 @@
 
 # Tools that support the plotters in this directory
 
+from datetime import datetime
+import glob
+import json
+import os
+import sys
+
+# The time format used in the JSON
+time_fmt = "%Y-%m-%d %H:%M:%S"
+# The Unix epoch
+epoch = datetime.utcfromtimestamp(0)
+
+START = 1
+STOP  = 2
+
 def abort(msg):
     print msg
     exit(1)
 
+def get_args():
+    """ Process input arguments """
+    if len(sys.argv) != 3:
+        abort("usage: <root directory> <output file>")
+    root_dir   = sys.argv[1]
+    output_file = sys.argv[2]
+    return (root_dir, output_file)
+
+def get_rundirs(root_dir):
+    """ Fill in rundirs, omitting miscellaneous files """
+    rundirs = []
+    entries = glob.glob(root_dir+"/*")
+    for entry in entries:
+        if os.path.isdir(entry):
+            rundirs.append(entry)
+    return rundirs
+
+def date2secs(date_string):
+    """Convert date string from JSON to floating-point seconds"""
+    tokens = date_string.split(".")
+    prefix = tokens[0]
+    suffix = tokens[1]
+    d = datetime.strptime(prefix, time_fmt)
+    secs = (d - epoch).total_seconds()
+    microsecs = int(suffix)
+    return secs+microsecs/1000000.0
+
+def get_json(rundir):
+    """ Find the JSON file for the given rundir """
+    output = rundir+"/output"
+    json_files = glob.glob(output+"/*.json")
+    count = len(json_files)
+    if count != 1:
+        abort("rundir: %s has %i JSON files!" % (rundir,count))
+    json_file = json_files[0]
+    # Open and parse the JSON file for start/stop events
+    with open(json_file, "r") as fp:
+        J = json.load(fp)
+    return J
